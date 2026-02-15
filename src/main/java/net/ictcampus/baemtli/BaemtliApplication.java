88cbf769a1d460fec9540a2f9020bfef3f8e7ad9
Jonas Vetsch <jonas@vetsch.com>
2026-02-15 17:48:00 +0100

    Clean project setup using start.spring.io

---
+++ src/main/java/net/ictcampus/baemtli/BaemtliApplication.java
@@ -0,0 +1,13 @@
+package net.ictcampus.baemtli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaemtliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaemtliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ictcampus/baemtli/BaemtliApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ictcampus.baemtli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaemtliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
